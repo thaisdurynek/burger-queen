c0b58c326539f8d507b07436eece492e03930852
Thais Durynek <thais.durynek@gmail.com>
2020-07-28 00:21:56 -0300

    :construction: estruturação dos dados do menu

---
+++ src/components/MenuData.js
@@ -0,0 +1,66 @@
+const menu = [
+    {
+        type: 'Café da Manhã',
+        item: 'Café Americano',
+        price: 5
+    },
+    {
+        type: 'Café da Manhã',
+        item: 'Café com Leite',
+        price: 7
+    },
+    {
+        type: 'Café da Manhã',
+        item: 'Misto Quente',
+        price: 10
+    },
+    {
+        type: 'Café da Manhã',
+        item: 'Suco Natural de Fruta',
+        price: 7
+    },
+    {
+        type: 'Hambúrgueres',
+        item: 'Hambúrguer Simples',
+        price: 10,
+        options: ['bovino', 'frango', 'vegetariano']
+    },
+    {
+        type: 'Hambúrgueres',
+        item: 'Hambúrguer Duplo',
+        price: 15,
+        options: ['bovino', 'frango', 'vegetariano']
+    },
+    {
+        type: 'Acompanhamentos',
+        item: 'Batata Frita',
+        price: 5
+    },
+    {
+        type: 'Acompanhamentos',
+        item: 'Anéis de Cebola',
+        price: 5
+    },
+    {
+        type: 'Bebidas',
+        item: 'Água 500mL',
+        price: 5
+    },
+    {
+        type: 'Bebidas',
+        item: 'Água 750mL',
+        price: 7
+    },
+    {
+        type: 'Bebidas',
+        item: 'Refrigerante 500mL',
+        price: 7
+    },
+    {
+        type: 'Bebidas',
+        item: 'Refrigerante 750mL',
+        price: 10
+    },
+];
+
+export default menu;
