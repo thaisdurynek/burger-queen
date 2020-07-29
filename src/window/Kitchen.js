09051e5172d4fb0869bf0cecb55dd81d31591974
Thais Durynek <thais.durynek@gmail.com>
2020-07-29 17:25:25 -0300

    :construction: array do menu excluído

--- src/window/Kitchen.js
+++ src/window/Kitchen.js
@@ -1,33 +1,30 @@
 import React from 'react';
 import Logo from '../components/Logo.js';
-import Menu from '../components/Menu.js';
+//import Menu from '../components/Menu.js';
 import MenuContainer from '../components/container/ContainerMenu.js';
-import Data from '../components/MenuData.js';
 import OptionsButton from '../components/OptionsButton.js';
-
-
+//import firebase from '../configs/FirebaseConfig.js';
 
 const Kitchen = () => {
   //const [changeMenu, setChangeMenu] = useState('');
-  const optionArray = (option) => {
-      Data.filter(elem => (elem.type === option));
-      console.log(Data);
+  const optionArray = (event) => {
+    event.preventDefault();
   }
   console.log(optionArray);
 
     return (
       <>
         <Logo text='Burger Queen'/>
-        <OptionsButton onClick={optionArray('Café da Manhã')} text='Café da Manhã' />
-        <OptionsButton onClick={optionArray('Principal')} text='Menu Principal' />
+        <OptionsButton onClick={optionArray} text='Café da Manhã' />
+        <OptionsButton text='Menu Principal' />
         <MenuContainer>
-          {Data.map(elem => (
+          {/* {Data.map(elem => (
             <Menu
             img={elem.img} 
             title={elem.item} 
             price={`${elem.price} R$`}
              />
-          ))}
+          ))} */}
         </MenuContainer>
       </>
     );
