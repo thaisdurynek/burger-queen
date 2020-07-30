d8a1cea06863d6a92336db082a4316204a11d1c1
Thais Durynek <thais.durynek@gmail.com>
2020-07-30 09:23:11 -0300

    :zap: menu renderizado do firebase

--- src/window/Kitchen.js
+++ src/window/Kitchen.js
@@ -1,34 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { withRouter } from 'react-router-dom';
 import Logo from '../components/Logo.js';
-//import Menu from '../components/Menu.js';
+import Menu from '../components/Menu.js';
 import MenuContainer from '../components/container/ContainerMenu.js';
 import OptionsButton from '../components/OptionsButton.js';
-//import firebase from '../configs/FirebaseConfig.js';
+import firebase from '../configs/FirebaseConfig.js';
 
 const Kitchen = () => {
-  //const [changeMenu, setChangeMenu] = useState('');
-  const optionArray = (event) => {
-    event.preventDefault();
-  }
-  console.log(optionArray);
+  const [menu, setMenu] = useState([]);
+
+  useEffect(()=>{
+    firebase.firestore().collection('Menu').onSnapshot((snapshot) => {
+      const newMenu = snapshot.docs.map((doc) => ({...doc.data()}))
+      setMenu(newMenu)
+    })
+  }, [])
 
     return (
       <>
         <Logo text='Burger Queen'/>
-        <OptionsButton onClick={optionArray} text='Café da Manhã' />
+        <OptionsButton text='Café da Manhã' />
         <OptionsButton text='Menu Principal' />
         <MenuContainer>
-          {/* {Data.map(elem => (
+          {menu.map(elem => (
             <Menu
             img={elem.img} 
             title={elem.item} 
             price={`${elem.price} R$`}
              />
-          ))} */}
+          ))}
         </MenuContainer>
       </>
     );
   };
   
-  export default Kitchen;
+  export default withRouter(Kitchen);
   
