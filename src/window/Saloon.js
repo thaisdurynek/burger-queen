1625802cee6bf8a9d00e6bf86bf82023cab5f328
Thais Durynek <thais.durynek@gmail.com>
2020-08-13 19:02:56 -0300

    :lipstick: estilização da tela de notificação feita

--- src/window/Saloon.js
+++ src/window/Saloon.js
@@ -16,6 +16,7 @@ import Header from '../components/container/Header';
 import BellNotNotified from '../assets/bell.png';
 import BellNotified from '../assets/bell-notification.png';
 import Notification from '../components/Notification';
+import Array from '../components/Array';
 
 const Saloon = (props) => {
   const [menu, setMenu] = useState([]);
@@ -33,28 +34,29 @@ const Saloon = (props) => {
   const [readyOrders, setReadyOrders] = useState([]);
 
   useEffect(() => {
-    firebase.firestore().collection('Menu').onSnapshot((snapshot) => {
-      const newMenu = snapshot.docs.map((doc) => ({ ...doc.data() }))
+    // firebase.firestore().collection('Menu').onSnapshot((snapshot) => {
+    //   const newMenu = snapshot.docs.map((doc) => ({ ...doc.data() }))
+      const newMenu = Array.map(arr => arr)
       setMenu(newMenu)
-    })
-  }, [menu, order, finalOrder, burgerInfo, burger, extra])
+    //})
+  }, [order, finalOrder, burgerInfo, burger, extra, readyOrders]);
 
   useEffect(() => {
-    firebase.firestore().collection('Orders').get().then((snapshot) => {
-      const orders = snapshot.docs.map((doc) => {
-        if (doc.data().stats === 'Encaminhado para o salão') {
-          return ({
-            id: doc.id,
-            ...doc.data()
-          })
-        } else {
-          return false
-        }
-      })
-      setReadyOrders(orders.filter(order => order !== false))
-    })
+    // firebase.firestore().collection('Orders').get().then((snapshot) => {
+    //   const orders = snapshot.docs.map((doc) => {
+    //     if (doc.data().status === 'Encaminhado para o salão') {
+    //       return ({
+    //         id: doc.id,
+    //         ...doc.data()
+    //       })
+    //     } else {
+    //       return false
+    //     }
+    //   })
+    //   setReadyOrders(orders.filter(elem => elem !== false))
+    // })
+    setReadyOrders([{status: "Encaminhado para o salão"}, {status: "Encaminhado para o salão"}])
   }, [])
-
 
   useEffect(() => {
     const timer = setTimeout(() => { setAlert('') }, 4000);
@@ -119,7 +121,7 @@ const Saloon = (props) => {
         order: order,
         observation: finalOrder.obs ? finalOrder.obs : '',
         price: total(order),
-        stats: "Encaminhado para a cozinha",
+        status: "Encaminhado para a cozinha",
         worker: user,
         date: new Date(),
       })
@@ -132,17 +134,15 @@ const Saloon = (props) => {
 
   const delivered = (event, id) => {
     event.preventDefault();
-    setReadyOrders(readyOrders.filter(order => order.id !== id))
     return firebase.firestore().collection('Orders').doc(id).update({
-      stats: 'Pedido Entregue!',
-      leadTime: new Date().toLocaleString('pt-BR'),
-      hall_time: new Date().getTime()
+      status: 'Pedido concluído!'
     });
   }
 
   const deleteItem = (e, key) => {
     e.preventDefault();
     order.splice(key, 1);
+    setOrder(order);
   }
 
   const total = (arr) => {
@@ -179,7 +179,7 @@ const Saloon = (props) => {
           right='1%' 
           width='34%' 
           height='50%'>
-            {readyOrders.map((elem) => (<Notification key={elem.id} table={elem.table} name={elem.name} onClick={(e) => delivered(e, elem.id)}/>))}
+            {readyOrders.map((elem) => (<Notification key={elem.id} table={elem.table} name={elem.name} order={elem.order.map(i => (`${i.item}, `))} onClick={(e) => delivered(e, elem.id)}/>))}
           </Container>): null}
         <Container direction='row' height='100%' mediaMargin='0'>
           <Container direction="column" 
